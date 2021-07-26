4ee05eb689591e83de6a03d1d06693e1a516a044
Thijs Heyndrickx <Thijs.heyndrickx@student.hogent.be>
2021-07-26 15:16:06 +0200

    homepage

---
+++ app/src/main/java/com/example/android/gintastic/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.example.android.gintastic.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
