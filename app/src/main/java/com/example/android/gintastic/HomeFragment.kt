20cb3db01beb7124f6be7e694319586d0a330309
Thijs Heyndrickx <Thijs.heyndrickx@student.hogent.be>
2021-08-05 23:09:45 +0200

    navigation

---
+++ app/src/main/java/com/example/android/gintastic/HomeFragment.kt
@@ -0,0 +1,32 @@
+package com.example.android.gintastic
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import com.example.android.gintastic.databinding.FragmentAllCocktailsBinding
+import com.example.android.gintastic.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding: FragmentHomeBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_home, container, false
+        )
+        binding.mainButtonAll.setOnClickListener {view: View ->
+            Navigation.findNavController(view).navigate(R.id.action_homeFragment_to_allCocktailsFragment)
+
+        }
+
+        return binding.root
+    }
+
+}
--- app/src/main/java/com/example/android/gintastic/MainActivity.kt
+++ app/src/main/java/com/example/android/gintastic/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.android.gintastic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.android.gintastic.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
